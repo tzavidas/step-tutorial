391e92297e18a38ac85545a7e173b2f0fcfcdf6e
Nick Tzavidas <tzavidas@google.com>
2020-07-24 08:44:37 +0000

    Removed the redundant getCommentsList() from the CommentsServlet

--- portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -25,25 +25,6 @@ public final class CommentsServlet extends HttpServlet {
     private CommentListSingleton commentList = CommentListSingleton.getInstance();
     private CommentBuilder commentBuilder = new CommentBuilder();
 
-    private List <Comment> getCommentsList() {
-        ArrayList <Comment> commentsList = new ArrayList <Comment>();
-
-        commentsList.add(new CommentBuilder()
-            .setId(1)
-            .setName("Max B")
-            .setDescription("My prev comment!")
-            .setPostDate(new Date(1595318478000L))
-            .build()
-        );
-
-        commentsList.add(new Comment(1, "Max B", "Hello y'all!", new Date(1595320478000L)));
-        commentsList.add(new Comment(2, "Maksim S", "Hey folks!", new Date(1595322478000L)));
-        commentsList.add(new Comment(3, "Remus N", "Hey Everyone!", new Date(1595324478000L)));
-        commentsList.add(new Comment(4, "Nick T", "Welcome everyone! Have fun! :)", new Date(1595326478000L)));
-
-        return commentsList;
-    }
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         List <Comment> allComments = commentList.getAllCommentsAsList();
